b70d5711397542d2bf72ead028394e520dc65ae2
cgs <cgs>
2019-10-01 21:38:33 +0900

    분산 프로세스 관리 컴포넌트 GPROC 추가
    - GPROC 소스코드 디렉토리, 파일 생성
    - 소스코드 저작권, 제작자 표시
    - Kconfig, Makefile 추가 및 수정

---
+++ linux-4.15/hcc/gproc/gproc.h
@@ -0,0 +1,12 @@
+/*
+ *  Copyright (C) 2019 Innogrid
+ */
+
+/*  writen by ish 2019 */
+
+#ifndef HCLOUDCLASSIC_KERNEL_ISH_GPROC_H
+#define HCLOUDCLASSIC_KERNEL_ISH_GPROC_H
+
+int init_gproc(void);
+
+#endif //HCLOUDCLASSIC_KERNEL_ISH_GPROC_H
