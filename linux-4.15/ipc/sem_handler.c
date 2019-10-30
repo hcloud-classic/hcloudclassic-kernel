9336ce5de49f2ea1eccf6f7b0140df2c8a2b8138
ish <ish@innogrid.com>
2019-10-30 14:11:36 +0900

    [Sem Handler] sem new array 기능 구현
    semaphore object 구조체 추가
    imported_sem => for remote sem
    local_sem => general sem
    mobile_sem_base => for remote sem base

--- linux-4.15/ipc/sem_handler.c
+++ linux-4.15/ipc/sem_handler.c
@@ -11,6 +11,7 @@
 #ifdef CONFIG_HCC_IPC
 #include "sem_handler.h"
 #include "util.h"
+#include "semarray_io_linker.h"
 #endif
 
 struct semhccops {
@@ -67,6 +68,51 @@ static void hcc_ipc_sem_unlock(struct kern_ipc_perm *ipcp)
 
 	rcu_read_unlock();
 }
+
+int hcc_ipc_sem_newary(struct ipc_namespace *ns, struct sem_array *sma)
+{
+	semarray_object_t *sem_object;
+	long *key_index;
+	int index ;
+
+	BUG_ON(!sem_ids(ns).hccops);
+
+	index = ipcid_to_idx(sma->sem_perm.id);
+
+
+
+	BUG_ON(sem_object);
+
+	sem_object = kmem_cache_alloc(semarray_object_cachep, GFP_KERNEL);
+	if (!sem_object)
+		return -ENOMEM;
+
+	sem_object->local_sem = sma;
+	sem_object->mobile_sem_base = NULL;
+	sem_object->imported_sem = *sma;
+
+	/* there are no pending objects for the moment */
+	BUG_ON(!list_empty(&sma->sem_pending));
+	BUG_ON(!list_empty(&sma->remote_sem_pending));
+
+	INIT_LIST_HEAD(&sem_object->imported_sem.list_id);
+	INIT_LIST_HEAD(&sem_object->imported_sem.sem_pending);
+	INIT_LIST_HEAD(&sem_object->imported_sem.remote_sem_pending);
+
+	_kddm_set_object(sem_ids(ns).hccops->data_kddm_set, index, sem_object);
+
+	if (sma->sem_perm.key != IPC_PRIVATE)
+	{
+		//Find key and set the value
+	}
+
+
+	sma->sem_perm.hccops = sem_ids(ns).hccops;
+
+	return 0;
+}
+
+
 
 
 static struct kern_ipc_perm *kcb_ipc_sem_findkey(struct ipc_ids *ids, key_t key)
